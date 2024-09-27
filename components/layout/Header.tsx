92da5f41a0a9f68528f864e0120ef2ea87b4225c
bilaly <billycsk@gmail.com>
2024-09-27 19:56:20 +0200

    Optmisation affichage home

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -25,6 +25,7 @@ export default function Header() {
 
 const styles = StyleSheet.create({
   container: {
+    marginTop: 24,
     flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center",
